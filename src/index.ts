33591fe43f7d45777cca0188e9076e799ece2a4e
Simon <szymon@kuffel.io>
2022-01-16 18:46:56 +0100

    fix: export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { I18Node as I18N } from './i18n'
 
-export const I18Node = I18N
+module.exports.I18Node = I18N
 
 const i18node = new I18N()
-export default i18node
+module.exports = i18node
