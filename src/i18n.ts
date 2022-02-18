b8229214fe1e734c059389b8302933dc46bc49a8
Simon <szymon@kuffel.io>
2022-02-18 18:18:10 +0100

    fix: refactor code

--- src/i18n.ts
+++ src/i18n.ts
@@ -5,35 +5,34 @@ import { fallbacksI, i18ndata } from './types'
 
 export class I18Node {
     locales: [string, string] = ['en', 'en']
-    directory: string | undefined = '../../../locales'
+    directory: string = '../../../locales'
     fallbacks: fallbacksI = {}
     warnDefaults: boolean = true
     warnMissingTranslations: boolean = false
 
-    constructor(data: i18ndata = { locales: ['en', 'en'], directory: '../../../locales', fallbacks: {}, warnDefaults: true, warnMissingTranslations: false }) {
+    constructor(data: i18ndata = { locales: undefined, directory: undefined, fallbacks: {}, warnDefaults: true, warnMissingTranslations: false }) {
         this.config(data)
     }
 
     config = (data: i18ndata) => {
-        if (data.warnDefaults == true) this.warnDefaults = true
-        if (data.locales == undefined && this.warnDefaults) console.warn('\x1b[33mNo locales specified\x1b[0m - defaults to ["en", "en"]')
-        if (data.directory == undefined && this.warnDefaults) console.warn('\x1b[31mA directory is required for storing JSON locale files\x1b[0m')
-        if (data.warnMissingTranslations == true) this.warnMissingTranslations = true
-
-        this.fallbacks = data.fallbacks
+        if (data.warnDefaults) {
+            if (data.locales == undefined) console.warn('\x1b[33mNo locales specified\x1b[0m - defaults to ["en", "en"]')
+            if (data.directory == undefined) console.warn('\x1b[31mA directory is required for storing JSON locale files\x1b[0m')
+        }
 
         if (data.locales != undefined) this.locales = data.locales
-        this.directory = data.directory
+        if (data.directory != undefined) this.directory = data.directory
+        if (data.fallbacks != undefined) this.fallbacks = data.fallbacks
+        this.warnDefaults = data.warnDefaults
+        this.warnMissingTranslations = data.warnMissingTranslations
 
         if (this.directory == undefined || !fs.existsSync(this.directory))
             throw new Error(`'\x1b[31m'Directory ${path.resolve(String(this.directory))} not found'\x1b[0m'`)
 
-        // Create missing locale JSON files
-
         if (!fs.existsSync(this.directory + `/${this.locales[0]}.json`)) fs.appendFileSync(this.directory + `/${this.locales[0]}.json`, '{}')
         if (!fs.existsSync(this.directory + `/${this.locales[1]}.json`)) fs.appendFileSync(this.directory + `/${this.locales[1]}.json`, '{}')
 
-        // Create missing locale JSON files for fallbacks
+        // Fallback JSON locale files
 
         var fallbackKeys: string[] = Object.keys(this.fallbacks)
         var fallbackLanguages: string[] = Object.values(this.fallbacks)
