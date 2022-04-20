810e048fb78ef1b3f45ae670b6ad4e60e7a9fe52
Simon <szymon@kuffel.io>
2022-04-20 13:42:48 +0200

    feat: check for config option types

--- src/i18n.ts
+++ src/i18n.ts
@@ -32,12 +32,22 @@ export class I18Node {
         if (!fs.existsSync(this.directory + `/${this.locales[0]}.json`)) fs.appendFileSync(this.directory + `/${this.locales[0]}.json`, '{}')
         if (!fs.existsSync(this.directory + `/${this.locales[1]}.json`)) fs.appendFileSync(this.directory + `/${this.locales[1]}.json`, '{}')
 
+        if (this.locales.length != 2) throw new Error('Config option "locales" must be an array of length 2')
+        if (typeof this.locales[0] != 'string' || typeof this.locales[1] != 'string') throw new Error('Config option "locales" must be a string array')
+        if (typeof this.directory != 'string') throw new Error('Config option "directory" must be type string')
+        if (typeof this.warnDefaults != 'boolean') throw new Error('Config option "warnDefaults" must be type boolean')
+        if (typeof this.warnMissingTranslations != 'boolean') throw new Error('Config option "warnMissingTranslations" must be type boolean')
+
         // Fallback JSON locale files
 
         var fallbackKeys: string[] = Object.keys(this.fallbacks)
         var fallbackLanguages: string[] = Object.values(this.fallbacks)
 
         for (var i: number = 0; i < fallbackKeys.length; i++) {
+            if (typeof fallbackKeys[i] != 'string')
+                throw new Error(`Invalid fallback locale '${fallbackKeys[i]}', must be type string, instead got type '${typeof fallbackKeys[i]}'`)
+            if (typeof fallbackLanguages[i] != 'string')
+                throw new Error(`Invalid fallback locale '${fallbackLanguages[i]}', must be type string, instead got type '${typeof fallbackLanguages[i]}'`)
             if (!fs.existsSync(this.directory + `/${fallbackKeys[i]}.json`)) fs.appendFileSync(this.directory + `/${fallbackKeys[i]}.json`, '{}')
             if (!fs.existsSync(this.directory + `/${fallbackLanguages[i]}.json`)) fs.appendFileSync(this.directory + `/${fallbackLanguages[i]}.json`, '{}')
         }
